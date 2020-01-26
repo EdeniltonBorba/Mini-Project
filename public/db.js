3517fe70d4fc9a89002c3e28471114ffd2a8eb80
Edenilton <edeniltonborba.adm@gmail.com>
2020-01-26 19:31:08 +0100

    db finished

--- public/db.js
+++ public/db.js
@@ -15,19 +15,20 @@ request.onsuccess = function(event) {
 };
 
 request.onerror = function(event) {
-  log error here
+  console.log("woops! " + event.target.errorCode);
 };
 
 function saveRecord(record) {
-  create a transaction on the pending db with readwrite access
-  access your pending object store
-  add record to your store with add method.
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  store.add(record);
 }
 
+
 function checkDatabase() {
-  open a transaction on your pending db
-  access your pending object store
-  get all records from store and set to a variable
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+  const getAll = store.getAll();
 
   getAll.onsuccess = function() {
     if (getAll.result.length > 0) {
@@ -41,13 +42,13 @@ function checkDatabase() {
       })
       .then(response => response.json())
       .then(() => {
-          if successful, open a transaction on your pending db
-          access your pending object store
-          clear all items in your store
+        const transaction = db.transaction(["pending"], "readwrite");
+        const store = transaction.objectStore("pending");
+        store.clear();
       });
     }
   };
 }
 
-listen for app coming back online
+
 window.addEventListener("online", checkDatabase);
