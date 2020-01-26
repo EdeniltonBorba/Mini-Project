4c62be0b8149e7e927db12ed779fc0d207fa135b
Edenilton <edeniltonborba.adm@gmail.com>
2020-01-26 19:20:03 +0100

    db js

--- public/db.js
+++ public/db.js
@@ -1,7 +1,5 @@
-check for indexedDB browser support
-
 let db;
-create a new db request for a "budget" database.
+const request = indexedDB.open("budget", 1);
 
 request.onupgradeneeded = function(event) {
   create object store called "pending" and set autoIncrement to true
